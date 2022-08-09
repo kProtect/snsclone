a9713f1268b2958e5a7bd386684c8f6e1bb773b9
Ben Smith <benlsmith329@gmail.com>
2022-08-09 12:47:13 -0400

    Payment First Push

--- Payment/routes/index.js
+++ Payment/routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', {
-    title: 'Bosta Payment Platform',
+    title: 'ChatterBridge',
     amount: process.env.AMOUNT,
     paypal_client_id: process.env.PAYPAL_CLIENT_ID
   });
