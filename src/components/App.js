107a14ef80da7fa43f7abfd02510c14f713fdd13
Lauren Anderson <laurenagraphicdesign@gmail.com>
2022-08-12 18:14:57 -0400

    unable to get login page to render. added queries and mutation on front end.

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import { Fragment, useEffect } from "react";
 import { connect } from "react-redux";
 import { handleInitialData } from "../actions/shared";
 import Nav from "./Nav";
+import Login from '../pages/Login';
 import DashBoard from "./Dashboard";
 import NewTweet from "./NewTweet"
 import TweetPage from "./TweetPage"
@@ -22,6 +23,8 @@ const App = (props) => {
                             <Route path="/" exact element={<DashBoard />}/>
                             <Route path="/tweet/:id" element={<TweetPage />}/>
                             <Route path="/add" exact element={<NewTweet />}/>
+                            <Route path="/login" element={<Login />}
+              />
                         </Routes>
                     )}
             </div>
